ee67e6ea61c8ba3f4894a5db020f507285d30c83
omrih <omrih@moonsite.co.il>
2018-03-14 18:32:31 +0200

    example ready

--- Example/App.js
+++ Example/App.js
@@ -9,6 +9,7 @@ import {
   Platform,
   StyleSheet,
   Text,
+  ScrollView,
   View
 } from 'react-native';
 import NumericInput from 'react-native-numeric-input'
@@ -22,39 +23,66 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView style={{paddingBottom:200}} contentContainerStyle={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native Numeric Input Example App!
+          Numeric Input Examples
         </Text>
         <Text style={styles.instructions}>
           Basic Numeric Input
         </Text>
-        <NumericInput onChange={value=>console.log(value)}/>
+        <NumericInput onChange={value => console.log(value)} />
+        <View style={styles.seprator}/>
         <Text style={styles.instructions}>
           Rounded Numeric Input
         </Text>
-        <NumericInput onChange={value=>console.log(value)} rounded/>
+        <NumericInput onChange={value => console.log(value)} rounded />
+        <View style={styles.seprator}/>
+
         <Text style={styles.instructions}>
           Basic Numeric Input - up {'&'} down
         </Text>
-        <NumericInput onChange={value=>console.log(value)} type='up-down'/>
+        <NumericInput onChange={value => console.log(value)} type='up-down' />
+        <View style={styles.seprator}/>
+
         <Text style={styles.instructions}>
           Rounded Numeric Input - up {'&'} down
         </Text>
-        <NumericInput onChange={value=>console.log(value)} rounded type='up-down'/>
-        <Text style={styles.instructions}>
-          Rounded Numeric Input - up {'&'} down with textColor:red iconColor:green
-        </Text>
-        <NumericInput onChange={value=>console.log(value)} rounded type='up-down' textColor='red' iconStyle={{color:'green'}} upDownButtonsBackgroundColor='red'/>
+        <NumericInput onChange={value => console.log(value)} rounded type='up-down' />
+        <View style={styles.seprator}/>
 
-      </View>
+        <Text style={styles.instructions}>
+          Rounded Numeric Input - up {'&'} down with styling
+        </Text>
+        <NumericInput onChange={value => console.log(value)} rounded type='up-down' textColor='#26547C' iconStyle={{ color: 'green' }} upDownButtonsBackgroundColor='#06D6A0' />
+        <View style={styles.seprator}/>
+
+        <Text style={styles.instructions}>
+          Rounded Numeric Input with styling
+        </Text>
+        <NumericInput onChange={value => console.log(value)} rounded textColor='#59656F' iconStyle={{ color: 'white' }} rightButtonBackgroundColor='#AC9FBB' leftButtonBackgroundColor='#DDBDD5' />
+        <View style={styles.seprator}/>
+
+
+        <Text style={styles.instructions}>
+          Rounded Numeric Input with costum width
+        </Text>
+        <NumericInput onChange={value => console.log(value)} totalWidth={200} rounded textColor='#103900' iconStyle={{ color: 'white' }} rightButtonBackgroundColor='#0FFF95' leftButtonBackgroundColor='#06BA63' />
+        <View style={styles.seprator}/>
+
+
+        <Text style={styles.instructions}>
+          Rounded Numeric Input with costum width and height
+        </Text>
+        <NumericInput onChange={value => console.log(value)} totalWidth={240} totalHeight={50} rounded textColor='#B0228C' iconStyle={{ color: 'white' }} rightButtonBackgroundColor='#EA3788' leftButtonBackgroundColor='#E56B70' />
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height:null,
+    width:null,
     justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -62,11 +90,17 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 60,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+    marginTop: 5,
   },
+  seprator:{
+    height:10,
+    width:200,
+    margin:10,
+  }
 });
