5d0255b935b2535e19d4d9169646b4b7b455ddd1
Erkan Ertürk <erkanerturk096@gmail.com>
2018-11-09 22:18:12 +0300

    Fix undefined this.calcSize(80)

--- NumericInput/NumericInput.js
+++ NumericInput/NumericInput.js
@@ -5,6 +5,8 @@ import Button from '../Button'
 import PropTypes from 'prop-types'
 import {create,PREDEF_RES} from 'react-native-pixel-perfect'
 
+const calcSize = create(PREDEF_RES.iphone7.px)
+
 export default class NumericInput extends Component {
     constructor(props) {
         super(props)
@@ -14,8 +16,6 @@ export default class NumericInput extends Component {
             stringValue: props.initValue.toString(),
         }
         this.ref = null
-        this.calcSize = create(PREDEF_RES.iphone7.px)
-
     }
 	
 	componentWillReceiveProps(props) {
@@ -28,7 +28,7 @@ export default class NumericInput extends Component {
     }
   }
     updateBaseResolution = (width,height) => {
-        this.calcSize = create({width,height})
+        calcSize = create({width,height})
     }
     inc = () => {
         let value = this.props.value && (typeof this.props.value === 'number') ? this.props.value : this.state.value
@@ -215,7 +215,7 @@ export default class NumericInput extends Component {
 const style = StyleSheet.create({
     seprator: {
         backgroundColor: 'grey',
-        height: this.calcSize(80),
+        height: calcSize(80),
     },
     inputContainerUpDown: {
         flexDirection: 'row',
@@ -246,7 +246,7 @@ const style = StyleSheet.create({
     },
     upDown: {
         alignItems: 'center',
-        paddingRight: this.calcSize(15)
+        paddingRight: calcSize(15)
     }
 })
 NumericInput.propTypes = {
@@ -278,10 +278,10 @@ NumericInput.propTypes = {
     reachMinDecIconStyle:PropTypes.any,
 }
 NumericInput.defaultProps = {
-    iconSize: this.calcSize(30),
+    iconSize: calcSize(30),
     borderColor: '#d4d4d4',
     iconStyle: {},
-    totalWidth: this.calcSize(220),
+    totalWidth: calcSize(220),
     sepratorWidth: 1,
     type: 'plus-minus',
     rounded: false,
