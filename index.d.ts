bb092fa4a6ccbc5609739dfb5bc5ea75a57db627
Francisco Batista <fbatista.fxbi@gmail.com>
2019-05-17 16:49:18 -0500

    Added type definition for Typescript

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+declare module 'react-native-numeric-input' {
+  import { ViewStyle } from 'react-native'
+  import { Color } from 'csstype'
+
+  export interface INumericInputProps {
+    value?: number
+    minValue?: number
+    maxValue?: number
+    step?: number
+    valueType?: 'integer' | 'real'
+    initValue: number
+    iconSize?: number
+    borderColor?: Color
+    iconStyle?: ViewStyle
+    totalWidth?: number
+    separatorWidth?: number
+    type?: 'plus-minus' | 'up-down'
+    rounded?: boolean
+    textColor?: Color
+    containerStyle?: ViewStyle
+    inputStyle?: ViewStyle
+    upDownButtonsBackgroundColor?: Color
+    rightButtonBackgroundColor?: Color
+    leftButtonBackgroundColor?: Color
+    totalHeight?: number
+    onChange: (value: number) => void
+    editable?: boolean
+    validateOnBlur?: boolean
+    reachMaxIncIconStyle?: ViewStyle
+    reachMaxDecIconStyle?: ViewStyle
+    reachMinIncIconStyle?: ViewStyle
+    reachMinDecIconStyle?: ViewStyle
+  }
+
+  export default class NumericInput extends React.Component<
+    INumericInputProps
+  > {}
+
+  export const DIMENTIONS: { height: number; width: number }
+}
